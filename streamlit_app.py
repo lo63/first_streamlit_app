a04745a6442f9ff08c8a6eb9a624ca7da39b9d45
lo63 <151757664+lo63@users.noreply.github.com>
2023-12-05 09:22:00 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,13 +20,12 @@ streamlit.dataframe(my_fruit_list)
 
 # Display the table on the page.
 
-
 #new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json)
+streamlit.text(fruityvice_response.json())
 
 
 
